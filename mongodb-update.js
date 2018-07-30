5aa4f5cd6fb5d4716029fbdd0808352e6ae48f72
CorpseDead <corpsedeads@gmail.com>
2018-07-30 15:05:09 +0200

    updated update

--- mongodb-update.js
+++ mongodb-update.js
@@ -7,10 +7,23 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (error, client) => {
     console.log("Connected to MongoDB server");
     const db = client.db("TodoApp");
 
-    db.collection("Todos").findOneAndUpdate({
-        _id: new ObjectID("5b5f067830cea1ec0d149231")
+    // db.collection("Todos").findOneAndUpdate({
+    //     _id: new ObjectID("5b5f067830cea1ec0d149231")
+    // }, {
+    //     $set: {completed: true}
+    // }, {
+    //     returnOriginal: false
+    // }).then(result => {
+    //     console.log(result);
+    // }, err => {
+    //     console.log("The update failed, ", err);
+    // });
+
+    db.collection("Users").findOneAndUpdate({
+        _id: new ObjectID("5b5ef6479f9a520963c29e00")
     }, {
-        $set: {completed: true}
+        $set: {title: "Legacy is but a hindrance"},
+        $inc: {age: 1}
     }, {
         returnOriginal: false
     }).then(result => {
